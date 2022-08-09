bc5abc9fe3965731f1655e7b8b428b94f727d683
adam <adam@test.com>
2022-08-09 15:59:55 +0300

    Add layout, css and js

---
+++ resources/views/Layouts/logo.blade.php
@@ -0,0 +1,4 @@
+<div class="logo">
+    <a href="https://www.creative-tim.com" class="simple-text logo-mini"></a>
+    <a href="https://www.creative-tim.com" class="simple-text logo-normal">Your Logo</a>
+</div>
