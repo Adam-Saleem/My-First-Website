45af8f53bd6b1001563b6fe42e5b41c5825657b3
adam <adam@test.com>
2022-08-18 13:31:57 +0300

    Add Pie chart for school page

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -1,10 +1,11 @@
-<?php
+<?php /** @noinspection ALL */
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
 use App\Http\Requests\SchoolRequest;
 use App\Models\School;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SchoolController extends Controller
 {
@@ -52,7 +53,23 @@ class SchoolController extends Controller
      */
     public function show(School $school)
     {
-        return view('school.show',compact('school'));
+        $labels = [];
+        $data = [];
+        $subjects = DB::table('subjects')
+            ->join('subject_teacher','subjects.id','=','subject_teacher.subject_id')
+            ->join('school_teacher','school_teacher.teacher_id','=','subject_teacher.teacher_id')
+            ->join('student_subject','subjects.id','=','student_subject.subject_id')
+            ->where('school_teacher.school_id', '=' , $school->id)
+            ->select('subjects.id','subjects.name')
+            ->selectRaw('count(student_subject.student_id) as student_count')
+            ->groupBy('subjects.id')
+            ->get();
+
+        foreach ($subjects as $subject){
+                    array_push($labels,$subject->name);
+                    array_push($data,$subject->student_count);
+        };
+        return view('school.show',compact('school','labels','data'));
     }
 
     /**
--- resources/views/school/show.blade.php
+++ resources/views/school/show.blade.php
@@ -8,14 +8,63 @@
                     <h5 class="title ">{{$school->name}}</h5>
                     <p class="description">{{$school->address}}</p>
                 </div>
-                <p>
-                    school description
-                </p>
-
+                <div>
+                    <canvas id="myChart" width="200" height="200"></canvas>
+                </div>
                 <div class="text-right">
                     <a href="{{ url("school/$school->id/edit") }}"><button class="btn btn-primary">Edit info</button></a>
                 </div>
             </div>
         </div>
-    </div>
+          </div>
+@endsection
+
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
+    <script>
+        let ctx = document.getElementById('myChart').getContext('2d');
+
+        let data = <?php echo json_encode($data); ?>;
+        let labels = <?php echo json_encode($labels); ?>;
+        let colorHex = ['#FB3640', '#EFCA08', '#43AA8B', '#253D5B'];
+        let myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                datasets: [{
+                    data: data,
+                    backgroundColor: colorHex
+                }],
+                labels: labels
+            },
+            options: {
+                responsive: true,
+                legend: {
+                    position: 'bottom'
+                },
+                plugins: {
+                    datalabels: {
+                        color: '#fff',
+                        anchor: 'end',
+                        align: 'start',
+                        offset: -10,
+                        borderWidth: 2,
+                        borderColor: '#fff',
+                        borderRadius: 25,
+                        backgroundColor: (context) => {
+                            return context.dataset.backgroundColor;
+                        },
+                        font: {
+                            weight: 'bold',
+                            size: '10'
+                        },
+                        formatter: (value) => {
+                            return value + ' %';
+                        }
+                    }
+                }
+            }
+        })
+    </script>
 @endsection
